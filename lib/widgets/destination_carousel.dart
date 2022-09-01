b76f469990c699aa8d7898bf437178a2e0d41536
theJohnCode <ifeanyichukwujohn70@gmail.com>
2022-09-01 17:17:56 +0100

    done with destination carousel

--- lib/widgets/destination_carousel.dart
+++ lib/widgets/destination_carousel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel_ui_app/models/destination_model.dart';
 
 class DestinationCarousel extends StatelessWidget {
@@ -33,9 +34,8 @@ class DestinationCarousel extends StatelessWidget {
                 ),
               ]),
         ),
-        Container(
+        SizedBox(
           height: 300,
-          color: Colors.blue,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemCount: destinations.length,
@@ -44,35 +44,38 @@ class DestinationCarousel extends StatelessWidget {
               return Container(
                 margin: const EdgeInsets.all(10),
                 width: 210,
-                color: Colors.red,
                 child: Stack(
+                  alignment: Alignment.topCenter,
                   children: <Widget>[
-                    Container(
-                      height: 120.0,
-                      width: 200.0,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${destination.activities.length} activities',
-                                style: const TextStyle(
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.w600,
-                                    letterSpacing: 1.2),
-                              ),
-                              Text(
-                                destination.description,
-                                style: const TextStyle(
-                                  color: Colors.grey,
+                    Positioned(
+                      bottom: 15,
+                      child: Container(
+                        height: 120.0,
+                        width: 200.0,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${destination.activities.length} activities',
+                                  style: const TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w600,
+                                      letterSpacing: 1.2),
                                 ),
-                              ),
-                            ]),
+                                Text(
+                                  destination.description,
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ]),
+                        ),
                       ),
                     ),
                     Container(
@@ -89,11 +92,48 @@ class DestinationCarousel extends StatelessWidget {
                       ),
                       child: Stack(
                         children: [
-                          Image(
-                            image: AssetImage(destination.imageUrl),
-                            height: 180,
-                            width: 180,
-                            fit: BoxFit.cover,
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image(
+                              image: AssetImage(destination.imageUrl),
+                              height: 180,
+                              width: 180,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Positioned(
+                            left: 10,
+                            bottom: 10,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  destination.city,
+                                  style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w600,
+                                      letterSpacing: 1.2,
+                                      color: Colors.white),
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      FontAwesomeIcons.locationArrow,
+                                      size: 10,
+                                      color: Colors.white,
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      destination.country,
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
---
+++ lib/widgets/hotel_carousel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HotelCarousel extends StatelessWidget {
+  const HotelCarousel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return
+  }
+}
